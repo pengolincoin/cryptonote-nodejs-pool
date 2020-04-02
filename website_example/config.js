b7f14135ac38589f0be96a48c005bb49dd91ca30
PengolinCoin <62501983+pengolincoin@users.noreply.github.com>
2020-04-02 14:30:19 +0200

    Update config.js

--- website_example/config.js
+++ website_example/config.js
@@ -1,17 +1,17 @@
-var api = "https://multi-miner.smartcoinpool.net:8119";
-let parentCoin = "Arqma"
+var api = "https://your_pool.com:8119"; //change address and API port
+let parentCoin = "PengolinCoin"
 
-var email = "support@poolhost.com";
-var telegram = "https://t.me/YourPool";
-var discord = "https://discordapp.com/invite/YourPool";
-var facebook = "https://www.facebook.com/<YourPoolFacebook>";
+var email = "Your_EMAIL"; // change to your e-mail
+var telegram = "Your_TELEGRAM"; // change to your Telegram or leave blank
+var discord = "Your_DISCORD"; // change to your Discord or leave blank
+var facebook = "Your_FACEBOOK"; // change to your Facebook or leave blank
 
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-LTC", "{symbol}-DOGE", "{symbol}-USDT", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
-var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
-var blockchainExplorerMerged = "http://explorer.ird.cash/?hash={id}#block";
-var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
-var transactionExplorerMerged = "http://explorer.ird.cash/?hash={id}#transaction";
+var blockchainExplorer = "http://peng-explorer.s2ii.xyz/?hash={id}#blockchain_block";
+//var blockchainExplorerMerged = "http://explorer.ird.cash/?hash={id}#block";
+var transactionExplorer = "http://peng-explorer.s2ii.xyz/?hash={id}#blockchain_transaction";
+//var transactionExplorerMerged = "http://explorer.ird.cash/?hash={id}#transaction";
 
 var themeCss = "themes/default.css";
 var defaultLang = 'en';
